8d154509c8956b53e9f63dd1cdb08b551b4684ce
mysticfalconvt <rboskind@gmail.com>
2020-10-08 18:01:14 -0400

    adding bullying forms

---
+++ models/Bullying.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+const bullyingSchema = new mongoose.Schema({
+  dateReported: {
+    type: Date,
+    default: Date.now,
+  },
+  author: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: "You must supply a teacher!",
+  },
+  offender: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: "You must supply a student!",
+  },
+  witnessed: String,
+  studentReporter: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+  },
+  incidentDate: Date,
+
+  description: {
+    type: String,
+  },
+  studentWitness: String,
+  employeeWitness: String,
+  studentsInterviewed: String,
+  initialActions: String,
+  nextSteps: String,
+});
+
+function autopopulate(next) {
+  this.populate("author");
+  this.populate("offender");
+  next();
+}
+
+bullyingSchema.pre("find", autopopulate);
+bullyingSchema.pre("findOne", autopopulate);
+
+module.exports = mongoose.model("Bullying", bullyingSchema);
