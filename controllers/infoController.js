1040b40d5f1a8d4853ea431719af79b1cb52bd10
mysticfalconvt <rboskind@gmail.com>
2021-02-22 13:42:26 -0500

    fixed a typo

--- controllers/infoController.js
+++ controllers/infoController.js
@@ -1,43 +1,43 @@
-const mongoose = require("mongoose");
-const { isStaff } = require("../handlers/permissions");
-const Info = mongoose.model("Info");
-const User = mongoose.model("User");
-const DashboardLinks = mongoose.model("DashboardLinks");
+const mongoose = require('mongoose');
+const { isStaff } = require('../handlers/permissions');
+const Info = mongoose.model('Info');
+const User = mongoose.model('User');
+const DashboardLinks = mongoose.model('DashboardLinks');
 
 // General Links Page Links
 
 exports.addInfo = (req, res) => {
-  res.render("editInfo", { title: "Add Important Info" });
+  res.render('editInfo', { title: 'Add Important Info' });
 };
 
 exports.getInfo = async (req, res) => {
-  const category = req.params.category || "category";
+  const category = req.params.category || 'category';
   let sort = {};
   sort[category] = 1;
   let infos = {};
   if (req.user) {
     // check if teacher for calendar events
     if (isStaff(req.user)) {
-      infos = await Info.find({ deleted: { $ne: "true" } }).sort(sort);
+      infos = await Info.find({ deleted: { $ne: 'true' } }).sort(sort);
     } else {
       infos = await Info.find({
-        teachersOnly: "",
-        deleted: { $ne: "true" },
+        teachersOnly: '',
+        deleted: { $ne: 'true' },
       }).sort(sort);
     }
   } else {
     infos = await Info.find({
-      teachersOnly: "",
-      deleted: { $ne: "true" },
+      teachersOnly: '',
+      deleted: { $ne: 'true' },
     }).sort({ category: 1 });
   }
-  res.render("infos", { title: "Important Links & Documents", infos: infos });
+  res.render('infos', { title: 'Important Links & Documents', infos: infos });
 };
 
 exports.createInfo = async (req, res) => {
   req.body.author = req.user.id;
   const info = await new Info(req.body).save();
-  req.flash("success", `Successfully Created ${info.title}.`);
+  req.flash('success', `Successfully Created ${info.title}.`);
   res.redirect(`/info/search/category`);
 };
 
@@ -48,12 +48,12 @@ exports.updateInfo = async (req, res) => {
     runValidators: true,
   }).exec();
   // redirect to the store and tell them it worked
-  req.flash("success", `Sucessfully Updated <strong>${info.title}</strong>.`);
+  req.flash('success', `Sucessfully Updated <strong>${info.title}</strong>.`);
   res.redirect(`/info/search/category`);
 };
 const confirmOwner = (calendar, user) => {
   if (!calendar.author.equals(user._id)) {
-    throw Error("You must own an event in order to edit it!");
+    throw Error('You must own an event in order to edit it!');
   }
 };
 exports.editInfo = async (req, res) => {
@@ -61,12 +61,12 @@ exports.editInfo = async (req, res) => {
   const info = await Info.findOne({ _id: req.params._id });
 
   //confirm they are owner of the event or admin
-  if (!req.user.permissions.include("admin")) {
+  if (!req.user.permissions.includes('admin')) {
     confirmOwner(info, req.user);
   }
 
   //render out the edit form so they can edit
-  res.render("editInfo", { title: `edit ${info.title}`, info });
+  res.render('editInfo', { title: `edit ${info.title}`, info });
 };
 
 // API
@@ -78,14 +78,14 @@ exports.searchInfos = async (req, res) => {
       $text: {
         $search: req.query.q,
       },
-      $or: [{ teachersOnly: teacher }, { teachersOnly: "" }],
+      $or: [{ teachersOnly: teacher }, { teachersOnly: '' }],
     },
     {
-      score: { $meta: "textScore" },
+      score: { $meta: 'textScore' },
     },
   )
     .sort({
-      score: { $meta: "textScore" },
+      score: { $meta: 'textScore' },
     })
     .limit(10);
   res.json(infos);
@@ -93,8 +93,8 @@ exports.searchInfos = async (req, res) => {
 
 // Dashboard Page Links
 exports.addDashboardLink = (req, res) => {
-  res.render("dashboardLinksForm", {
-    title: "Add New Link to Dashboard",
+  res.render('dashboardLinksForm', {
+    title: 'Add New Link to Dashboard',
     dashboardLinks: {},
   });
 };
@@ -102,7 +102,7 @@ exports.addDashboardLink = (req, res) => {
 exports.createDashboardLink = async (req, res) => {
   req.body.author = req.user.id;
   const info = await new DashboardLinks(req.body).save();
-  req.flash("success", `Successfully Created ${info.title}.`);
+  req.flash('success', `Successfully Created ${info.title}.`);
   res.redirect(`/dashboardLinks`);
 };
 
@@ -111,24 +111,24 @@ exports.getDashboardLink = async (req, res) => {
     // check if teacher for calendar events
     if (isStaff(req.user)) {
       const dashboardLinks = await DashboardLinks.find({
-        deleted: { $ne: "true" },
+        deleted: { $ne: 'true' },
       });
       console.log(dashboardLinks);
 
       links = await DashboardLinks.find({
         $and: [
           { permissions: { $in: req.user.permissions } },
-          { deleted: { $ne: "true" } },
+          { deleted: { $ne: 'true' } },
         ],
       });
 
-      res.render("dashboardLinks", {
-        title: "List of links for dashboard page",
+      res.render('dashboardLinks', {
+        title: 'List of links for dashboard page',
         dashboardLinks,
       });
     }
   } else {
-    res.redirect("/");
+    res.redirect('/');
   }
 };
 
@@ -139,7 +139,7 @@ exports.editDashboardLink = async (req, res) => {
   //confirm they are owner of the event or admin
 
   //render out the edit form so they can edit
-  res.render("dashboardLinksForm", {
+  res.render('dashboardLinksForm', {
     title: `edit ${dashboardLinks.title}`,
     dashboardLinks,
   });
@@ -156,6 +156,6 @@ exports.updateDashboardLink = async (req, res) => {
     },
   ).exec();
   // redirect to the store and tell them it worked
-  req.flash("success", `Sucessfully Updated <strong>${info.title}</strong>.`);
+  req.flash('success', `Sucessfully Updated <strong>${info.title}</strong>.`);
   res.redirect(`/`);
 };
